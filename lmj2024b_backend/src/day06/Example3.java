2fa9bc5fc130a0da99433a6cc0e6fd04309f4912
leeminjin0827 <lmj98202@gmail.com>
2024-12-16 16:01:42 +0900

    4:00

---
+++ lmj2024b_backend/src/day06/Example3.java
@@ -0,0 +1,17 @@
+package day06;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+		
+		// 1분복습 : 1. class 2. Person 3. int
+		// 생각해보기 : 생활속의 객체들을 클래스 코드로 표현하기
+			// - 학생 , 사람, 자동차, 주문 제외한다.
+			// - 작성후 카톡방에 제출
+		class soccer{ String 포지션; String 선수; String 감독; String 전술; }
+		
+		
+		
+	} // m end
+} // c end
---
+++ lmj2024b_backend/src/day06/Sudent.java
@@ -0,0 +1,9 @@
+package day06;
+
+public class Sudent {
+	int stucentID; 			// 학번
+	String studentName;		// 학생이름
+	int grade;				// 학년
+	String address;			// 사는곳
+	
+}
